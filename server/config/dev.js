36e94225a05ca606e22303f627ccee1902c05422
atakanaytan <aytan.atakan2@gmail.com>
2020-08-22 01:00:46 +0300

    MongoDB connection has added, rentalSchema has created

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+
+
+module.exports = {
+    DB_URI: 'mongodb+srv://<user>:<password>@cluster0.qcs34.mongodb.net/<cluster>?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config/dev');
 
+// routes
 const rentalRoutes = require('./routes/rentals')
+
+//models
+const Rental = require('./models/rental');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => {
+    console.log('Connected to the DB');
+});
 
 // Middleware
 app.use(bodyParser.json())
