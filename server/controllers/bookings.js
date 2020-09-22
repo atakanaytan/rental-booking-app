cecda573466522093a91ff9565eb44e364529c6d
atakanaytan <aytan.atakan2@gmail.com>
2020-09-22 16:43:48 +0300

    Booking feature added. Saves booked rentals to the DB.

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -4,6 +4,28 @@ const Booking = require('../models/booking');
 
 exports.createBooking = (req, res) => {
    const bookingData = req.body;
-   
-   return res.json({message: 'Booking is created'});
-} 
+   const booking = new Booking({...bookingData, user: res.locals.user});
+
+   Booking.find({rental: booking.rental}, (error, rentalBookings) => {
+      if (error) { return res.mongoError(error); }
+
+      const isValid = checkIfBookingIsValid(booking);
+      if (isValid) {
+
+         booking.save((error, savedBooking) => {
+            if (error) { return res.mongoError(error); }
+            
+            return res.json({startAt: savedBooking.startAt, endAt: savedBooking.endAt})
+         })
+      } else {
+         return res.json({message: 'Booking is not created!'});
+      }
+   })
+
+    
+}
+
+function checkIfBookingIsValid(booking) {
+   return true;
+}
+
