fa41d8f7395d0bedd71ad53f792c986aab1d745f
atakanaytan <aytan.atakan2@gmail.com>
2020-09-25 15:52:36 +0300

    The modal component added before the reservation to check details.

--- src/components/booking/BookingReserve.js
+++ src/components/booking/BookingReserve.js
@@ -1,73 +1,115 @@
-
-
 import React from 'react';
 import DateRangePicker from 'react-bootstrap-daterangepicker';
-
+import RentalNowModal from 'components/shared/Modal';
+import moment from 'moment';
 
 class BookingReserve extends React.Component {
 
+  constructor() {
+    super();
+    this.dateRef = React.createRef();
 
-    constructor() {
-        super();
-        
-        this.state = {
-            proposedBooking: {
-                guests: ''                
-            }
-        }
+    this.state = {
+      proposedBooking: {
+        guests: '',
+        startAt: null,
+        endAt: null
+      }
     }
+  }
 
-    handleGuestsChange = (event) => {
-        this.setState({
-            proposedBooking: {
-                ...this.state.proposedBooking,
-                guests: event.target.value
-            }
-        })
-    }
+  handleApply = (_, {startDate, endDate}) => {
+    this.dateRef.current.value = moment(startDate).format('YYYY/MM/DD') + ' to ' + moment(endDate).format('YYYY/MM/DD')
 
-    reserveRental = () => {
-        alert(JSON.stringify(this.state.proposedBooking));
-    }
+    this.setState({
+      proposedBooking: {
+        ...this.state.proposedBooking,
+        startAt: startDate,
+        endAt: endDate
+      }
+    })
+  }
 
-    render() {
-        return(
-          <div className='booking'>
-            <h3 className='booking-price'>$ 12 <span className='booking-per-night'>per night</span></h3>
-            <hr></hr>
-            <div className='form-group'>
-              <label htmlFor='dates'>Dates</label>
-              <DateRangePicker
-                containerStyle={{display: 'block'}}>
-                <input
-                  id="dates"
-                  type="text"
-                  className="form-control">
-                </input>
-             </DateRangePicker>
-            </div>
-            <div className='form-group'>
-                <label htmlFor='guests'>Guests</label>
-                  <input
-                   onChange={this.handleGuestsChange}
-                    value={this.state.proposedBooking.guests} 
-                    type='number'
-                    className='form-control'
-                    id='guests'
-                    aria-describedby='guests'>
-                    </input>
-            </div>
-            <button 
-            onClick={this.reserveRental}
-            className='btn btn-bwm-main btn-block'>Reserve place now</button>
-            <hr></hr>
-              <p className='booking-note-title'>People are interested into this house</p>
-              <p className='booking-note-text'>
-                  More than 500 people checked this rental in last month.
-              </p>
-          </div>
-        )
-    }
+  checkInvalidDates = (date) => {
+    // if date is invalid return true
+    return date < moment().add(-1, 'days');
+  }
+
+  handleGuestsChange = (event) => {
+    // TODO: explain destracturizing
+    this.setState({
+      proposedBooking: {
+        ...this.state.proposedBooking,
+        guests: event.target.value
+      }
+    })
+  }
+
+  reserveRental = () => {
+    alert(JSON.stringify(this.state.proposedBooking));
+  }
+
+  get formattedDate() {
+    return this.dateRef.current ? this.dateRef.current.value : '';
+  }
+
+  render() {
+    const { rental } = this.props;
+    const { proposedBooking } = this.state;
+
+    return (
+      <div className='booking'>
+        <h3 className='booking-price'>$ {rental.dailyPrice} <span className='booking-per-night'>per night</span></h3>
+        <hr></hr>
+        <div className='form-group'>
+        <label htmlFor='dates'>Dates</label>
+        <DateRangePicker
+          onApply={this.handleApply}
+          opens="left"
+          containerStyles={{display: 'block'}}
+          isInvalidDate={this.checkInvalidDates}>
+          <input
+            ref={this.dateRef}
+            id="dates"
+            type="text"
+            className="form-control">
+          </input>
+        </DateRangePicker>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='guests'>Guests</label>
+          <input 
+            onChange={this.handleGuestsChange}
+            value={proposedBooking.guests}
+            type='number'
+            className='form-control'
+            id='guests'
+            aria-describedby='guests'>
+          </input>
+        </div>
+        <RentalNowModal
+          onSubmit={this.reserveRental}
+          title="Confirm Booking"
+          subtitle={this.formattedDate}
+          openBtn={
+            <button
+              className='btn btn-bwm-main btn-block'>Reserve place now
+            </button>}
+          >
+            <em>12</em> Nights /
+            <em> ${rental.dailyPrice}</em> per Night
+            <p>Guests: <em>{proposedBooking.guests}</em></p>
+            <p>Price: <em>$200</em></p>
+            <p>Do you confirm your booking for selected days?</p>
+        </RentalNowModal>
+        <hr></hr>
+        <p className='booking-note-title'>People are interested into this house</p>
+        <p className='booking-note-text'>
+          More than 500 people checked this rental in last month.
+        </p>
+      </div>
+    )
+  }
 }
 
 export default BookingReserve;
---
+++ src/components/shared/Modal.js
@@ -0,0 +1,52 @@
+
+import React, { useState } from 'react';
+import 'react-responsive-modal/styles.css'
+import { Modal } from 'react-responsive-modal'
+
+
+const RentalNowModal = ({
+    title = "Modal Window", 
+    subtitle="Confirm your data",
+    openBtn: OpenBtn, 
+    onSubmit,
+    children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+          { !OpenBtn &&
+            <button
+              onClick={() => setIsOpen(true)}
+              className="btn btn-success">Open</button>
+         }
+         { OpenBtn &&
+           <div onClick={() => setIsOpen(true)}>
+              {OpenBtn}
+            </div>
+         } 
+          <Modal 
+            focusTrapped={false}
+            open={isOpen}
+            onClose={() => setIsOpen(false)} 
+            classNames={{ modal: 'rental-modal' }}>
+          <h4 className='modal-title title'>{title}</h4>
+          <p className='modal-subtitle'>{subtitle}</p>
+          <div className='modal-body'>
+            {children}
+          </div>
+          <div className='modal-footer'>
+            <button
+              onClick={onSubmit} 
+              type='button' 
+              className='btn btn-bwm-main'>Confirm</button>
+            <button 
+              onClick={() => setIsOpen(false)}
+              type='button' 
+              className='btn btn-alert'>Cancel</button>
+          </div>
+          </Modal>
+        </>
+      )
+}
+
+export default RentalNowModal;
