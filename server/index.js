a1091cb4daa32ae7d7cc9cd87e819110894e7941
atakanaytan <aytan.atakan2@gmail.com>
2020-11-12 01:36:25 +0300

    Production setup

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const config = require('./config');
 const { provideErrorHandler } = require('./middlewares');
+const path = require('path');
 
 // routes
 const rentalRoutes = require('./routes/rentals');
@@ -43,6 +44,15 @@ app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 app.use('/api/v1/image-upload', imageUploadRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const buildPath = path.join(__dirname, '..', 'build');
+  app.use(express.static(buildPath));
+  app.get('*', (req, res) => {
+    return res.sendFile(path.resolve(buildPath, 'index.html'));
+  })
+}
+ 
 
 app.listen(PORT, () => {
     console.log('Server is listening on port: ', PORT);
